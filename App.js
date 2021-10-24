520ec84928815369ae79b958698dfa71cd794b1f
Mohd Sameer Ahmad <mohdsameer7408@gmail.com>
2021-10-24 18:29:15 +0530

    added OnBoarding screens

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Image, StyleSheet } from "react-native";
+import OnBoading from "react-native-onboarding-swiper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <OnBoading
+      onDone={() => console.log("login")}
+      controlStatusBar={false}
+      pages={[
+        {
+          backgroundColor: "#a6e4d0",
+          image: (
+            <Image
+              source={require("./assets/travel1.png")}
+              resizeMode="contain"
+              style={styles.boardingImage}
+            />
+          ),
+          title: "Hello World",
+          subtitle:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, suscipit!",
+        },
+        {
+          backgroundColor: "#fd3b93",
+          image: (
+            <Image
+              source={require("./assets/travel2.png")}
+              resizeMode="contain"
+              style={styles.boardingImage}
+            />
+          ),
+          title: "Hello World",
+          subtitle:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, suscipit!",
+        },
+      ]}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  boardingImage: {
+    width: "100%",
+    height: 200,
   },
 });
